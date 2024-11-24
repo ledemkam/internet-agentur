96bcddd2103ab12368d2f2656bcc566f8dc10ab4
ledemkam <ledemkam@gmail.com>
2024-11-24 14:44:15 +0100

    finisch Home implment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
+import DynamicText from "../components/DynamicText"
 import Mouse from "../components/Mouse"
 import Navigation from "../components/Navigation"
 import SocialNetwork from "../components/SocialNetwork"
 
 export default function Home() {
   return (
-    <div>
+    <>
+    <div className="home">
       <Mouse />
       <Navigation />
       <SocialNetwork />
+      <div className="home-main">
+        <div className="main-content">
+          <h1>FS AGENCY</h1>
+          <h2><DynamicText/></h2>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
